8af3ffe8671b64fd1d69d584bf0a2bc3889b599f
Siyanthan Vijithamparanathan <vijisiyanthan@gmail.com>
2020-06-03 19:43:23 -0400

    Create command line program to get player fantasy scores vs a specific team

---
+++ fantasysites.py
@@ -0,0 +1,8 @@
+class Fanduel():
+    scoring = 1
+    rebound = 1.2
+    assist = 1.5
+    block = 3
+    steal = 3
+    turnover = -1
+
--- vsTeam.py
+++ vsTeam.py
@@ -1,2 +1,36 @@
 from basketball_reference_web_scraper import client
+from sys import argv
+from fantasysites import Fanduel
+
+## Find player matchup history vs opponent
+
+def calculate_fantasy_value(boxscore):
+
+    fantasyvalue = boxscore['points_scored'] * Fanduel.scoring + \
+                   boxscore['steals'] * Fanduel.steal + \
+                   boxscore['blocks'] * Fanduel.block + \
+                   boxscore['defensive_rebounds'] * Fanduel.rebound + \
+                   boxscore['offensive_rebounds'] * Fanduel.rebound - \
+                   boxscore['turnovers'] * Fanduel.turnover
+    return fantasyvalue
+
+
+if __name__ == '__main__':
+    if len(argv) == 4:
+        name = f"{argv[1]} {argv[2]}"
+        opp = f"{argv[3]}"
+        player = client.search(term=name)
+        players = player["players"]
+        for play in players:
+            print(play['identifier'])
+            for i in range(10):
+                boxscores = client.regular_season_player_box_scores(
+                    player_identifier=play['identifier'],
+                    season_end_year=2010 + i,
+                )
+                for game in boxscores:
+                    if game['opponent'].name == opp:
+                        print(calculate_fantasy_value(game))
+
+
 
